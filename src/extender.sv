ae8c4c5497bdc36fa1ef6ac7db375e7272c8ec50
MANAV SHAH <manavbshah28@gmail.com>
2025-12-18 11:38:25 -0500

    extender

--- src/extender.sv
+++ src/extender.sv
@@ -1,6 +1,14 @@
 import riscky_pkg::*;
 
 module extender(
+    input [ILEN-1:0] instr,
+    input [1:0] imm_sel,
+    output [XLEN-1:0] imm_ext
 );
 
+assign imm_ext = (imm_sel == 2'b00) ? {{(XLEN-12){instr[31]}}, instr[31:20]} : // I type
+                 (imm_sel == 2'b01) ? {{(XLEN-12){instr[31]}}, instr[31:25], instr[11:7]} : // S type
+                 (imm_sel == 2'b10) ? {{(XLEN-12){instr[31]}}, instr[7]    , instr[30:25] , instr[11:8] ,1'b0}: // B type
+                                      {{(XLEN-21){instr[31]}}, instr[19:12], instr[20]    , instr[30:21],1'b0} ; // J type
+                                      
 endmodule
